8badb32fb82efcabbfad71d0f32e8d4ec4f8b2bb
Ishmael Akaboa <ishmaelakaboa@gmail.com>
2025-02-04 14:25:18 +0000

    initialize operation sign variable

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func main(){
 	var a, b float64
+	var operation string
+	
 	fmt.Print("Enter first number:")
 	fmt.Scan(&a)
 	fmt.Print("Enter second number:")
 	fmt.Scan(&b)
 	fmt.Print("Enter operation (+, - , /, *):")
-	fmt.Println()
+	fmt.Print(&operation)
 }
