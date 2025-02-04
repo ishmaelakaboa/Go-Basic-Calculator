edb074ff96c265583292ac46fb3ff241df5ab520
Ishmael Akaboa <ishmaelakaboa@gmail.com>
2025-02-04 14:23:51 +0000

    operational sign input

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main(){
 	var a, b float64
-	fmt.Print("Enter first number")
+	fmt.Print("Enter first number:")
 	fmt.Scan(&a)
-	fmt.Print("Enter second number")
+	fmt.Print("Enter second number:")
 	fmt.Scan(&b)
+	fmt.Print("Enter operation (+, - , /, *):")
 	fmt.Println()
 }
